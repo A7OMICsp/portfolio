5d72fbcae5f885b11871c56dee1c50f1a6659584
Alex <amoscardovilar@gmail.com>
2023-09-27 17:17:26 +0200

    latest changes

---
+++ src/data/skills.ts
@@ -0,0 +1,64 @@
+export interface ISkill {
+    name: string;
+    imgUrl: string;
+}
+
+export const skillsArray = [
+    {
+        name: 'HTML',
+        imgUrl: 'https://cdn.svgporn.com/logos/html-5.svg'
+    },
+    {
+        name: 'CSS',
+        imgUrl: 'https://cdn.svgporn.com/logos/css-3.svg'
+    },
+    {
+        name: 'JavaScript',
+        imgUrl: 'https://cdn.svgporn.com/logos/javascript.svg'
+    },
+    {
+        name: 'TypeScript',
+        imgUrl: 'https://cdn.svgporn.com/logos/typescript-icon.svg'
+    },
+    {
+        name: 'React',
+        imgUrl: 'https://cdn.svgporn.com/logos/react.svg'
+    },
+    {
+        name: 'Astro',
+        imgUrl: 'https://cdn.svgporn.com/logos/astro.svg'
+    },
+    {
+        name: 'Angular',
+        imgUrl: 'https://cdn.svgporn.com/logos/angular-icon.svg'
+    },
+    {
+        name: 'Ionic',
+        imgUrl: 'https://cdn.svgporn.com/logos/ionic-icon.svg'
+    },
+    {
+        name: 'NodeJS',
+        imgUrl: 'https://cdn.svgporn.com/logos/nodejs-icon.svg'
+    },
+    {
+        name: 'Express',
+        imgUrl: 'https://cdn.svgporn.com/logos/express.svg'
+    },
+    {
+        name: 'MySQL',
+        imgUrl: 'https://cdn.svgporn.com/logos/mysql.svg'
+    },
+    {
+        name: 'C#',
+        imgUrl: 'https://cdn.svgporn.com/logos/c-sharp.svg'
+    },
+    {
+        name: 'Java',
+        imgUrl: 'https://cdn.svgporn.com/logos/java.svg'
+    },
+    {
+        name: '.NET',
+        imgUrl: 'https://cdn.svgporn.com/logos/dotnet.svg'
+    }
+        
+];
